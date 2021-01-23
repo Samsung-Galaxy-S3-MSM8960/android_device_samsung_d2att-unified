5da20939546e766f1ba26e0d1fa8117829700dde
alexenferman <alextud0132@gmail.com>
2021-01-22 19:33:44 -0500

    Move build fingerprint and add Inherits

--- lineage.mk
+++ lineage.mk
@@ -1,11 +1,14 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/d2att/device.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk) 
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2att \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNJ2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNJ2 release-keys"
+    
+BUILD_FINGERPRINT := samsung/lineage_d2att/d2att:9/I747UCUFNJ2/alexenferman:userdebug/release-keys
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := lineage_d2att
